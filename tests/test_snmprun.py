ae0b36dd75349d74848121bf193c1c61f5cf5649
Yakov Shiryaev <yman@protonmail.ch>
2015-12-24 23:06:52 +0700

    add some SNMP tests

--- tests/test_snmprun.py
+++ tests/test_snmprun.py
@@ -9,21 +9,52 @@ class DefaultTest(unittest.TestCase):
 
 
 class SnmpTest(unittest.TestCase):
-    def test_snmp_get(self):
-        config = {}
+    def setUp(self):
+        self.config = {}
         with open('tests/test.conf') as test_conf:
             for line in test_conf:
-                config[line.split('=')[0]] = line.split('=')[1].strip()
-        engine = SnmpEngine()
-        snmp_get = utils.snmpget.snmp_run(engine, config['community'],
-                                          config['ip'], config['fw_oid'])
+                self.config[line.split('=')[0]] = line.split('=')[1].strip()
+        self.engine = SnmpEngine()
+
+    def test_snmp_get(self):
+        snmp_get = utils.snmpget.snmp_run(self.engine, self.config['community'],
+                                          self.config['ip'],
+                                          self.config['fw_oid'])
         er_indication, er_status, er_index, var_binds = next(snmp_get)
         full_oid, firmware = utils.snmpget.process_output(
-            er_indication, er_status, er_index, var_binds, config['ip'])
-        self.assertEqual(firmware, config['fw'])
-        full_oid, sw_model = utils.snmpget.get_with_send(config['model_oid'],
-                                                         config['ip'], snmp_get)
-        self.assertEqual(sw_model, config['sw'])
+            er_indication, er_status, er_index, var_binds, self.config['ip'])
+        self.assertEqual(firmware, self.config['fw'])
+        full_oid, sw_model = utils.snmpget.get_with_send(
+            self.config['model_oid'], self.config['ip'], snmp_get)
+        self.assertEqual(sw_model, self.config['sw'])
+
+    def test_snmp_bulkget(self):
+        count = 0
+        snmp_get = utils.snmpget.snmp_run(self.engine, self.config['community'],
+                                          self.config['ip'], 'ifAlias',
+                                          mib='IF-MIB', action='bulk')
+        for er_indication, er_status, er_index, var_binds in snmp_get:
+            count += 1
+        self.assertTrue(count > 1)
+
+    def test_snmp_next(self):
+        count = 0
+        snmp_get = utils.snmpget.snmp_run(self.engine, self.config['community'],
+                                          self.config['ip'], 'ifOperStatus',
+                                          mib='IF-MIB', action='next', index=5)
+        while count < 10:
+            er_indication, er_status, er_index, var_binds = next(snmp_get)
+            count += 1
+        self.assertEqual(count, 10)
+
+    def test_snmp_walk(self):
+        count = 0
+        snmp_get = utils.snmpget.tree_walk(
+            self.engine, self.config['community'], self.config['ip'],
+            'ifAdminStatus', mib='IF-MIB')
+        for oid, value in snmp_get:
+            count += 1
+        self.assertIn('1.3.6.1.2.1.2.2.1.7', oid)
 
 if __name__ == '__main__':
     unittest.main()
